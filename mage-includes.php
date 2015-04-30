9a0e7cfb5e7d231cb7f9f3ac0a20cac091113c90
Josh Di Fabio <josh.difabio@ampersandit.co.uk>
2015-04-30 11:54:25 +0100

    Merge vendor-autoload.php into mage-includes.php

--- mage-includes.php
+++ mage-includes.php
@@ -1,5 +1,10 @@
 <?php
-$autoloader = require __DIR__ . '/vendor-autoload.php';
+if (!$autoloader = @include dirname(dirname(__DIR__)) . '/autoload.php') {
+    if (!$autoloader = @include __DIR__ . '/vendor/autoload.php') {
+        throw new \RuntimeException('Failed to load Composer autoloader.');
+    }
+}
+
 $autoloader->unregister();
 
 $ds = DIRECTORY_SEPARATOR;
