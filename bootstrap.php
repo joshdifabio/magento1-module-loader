320465edb9d2c556764cb14338ba29b2c0398a1b
Josh Di Fabio <josh.difabio@ampersandit.co.uk>
2015-05-08 16:21:09 +0100

    Revert "Remove mage-includes.php"
    This reverts commit 5f654f11afd4a1adc623ee36b03d02f9a2e87b51.

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,6 @@
 <?php
 namespace MagentoModuleLoader;
 
-if (!class_exists('Mage', false)) {
-    return;
-}
-
 if (!$autoloader = @include dirname(dirname(__DIR__)) . '/autoload.php') {
     if (!$autoloader = @include __DIR__ . '/vendor/autoload.php') {
         throw new \RuntimeException('Failed to load Composer autoloader.');
@@ -12,6 +8,10 @@ if (!$autoloader = @include dirname(dirname(__DIR__)) . '/autoload.php') {
 }
 
 $autoloader->unregister();
+
+if (!class_exists('Mage', false)) {
+    require __DIR__ . '/mage-includes.php';
+}
 
 ClassRewriter::loadClassAs('Mage_Core_Model_Config', 'Mage_Core_Model_Config__vanilla');
 
